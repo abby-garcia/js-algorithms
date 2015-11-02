f14a98518273632e21c3d72cb3e5cdaa67c52ebe
abby-garcia <acg361@gmail.com>
2015-11-02 00:06:06 -0500

    added more questions

---
+++ 6. letter capitalize/capitalize.js
@@ -0,0 +1,4 @@
+Using the JavaScript language, have the function LetterCapitalize(str) 
+ke the str parameter being passed and capitalize the first 
+letter of each word. 
+Words will be separated by only one space. 
