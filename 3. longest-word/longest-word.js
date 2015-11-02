3c6d11fc2e7883e14aabd456a22c60d8054fbb9d
abby-garcia <acg361@gmail.com>
2015-11-02 15:30:55 -0500

    answer question 3

--- 3. longest-word/longest-word.js
+++ 3. longest-word/longest-word.js
@@ -4,3 +4,14 @@ the largest word in the string. If there are two or
  more words that are the same length, return the first 
  word from the string with that length. 
 Ignore punctuation and assume sen will not be empty. 
+
+
+function LongestWord(sen){
+	var rules = sen.match(/[a-z0-9]+/gi);       // you need to set the rules of what you're looking for first
+	var sorted = rules.sort(function(a,b) {      // then you need to think of how to want it sorted. 
+		return b.length - a.length;
+	});     
+		return sorted[0];
+}
+
+LongestWord("What is the longgggestttttttttt word here?");
