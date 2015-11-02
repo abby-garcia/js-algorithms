d58e4b660bd4f36b096de94628611a5dae70a45d
abby-garcia <acg361@gmail.com>
2015-11-02 11:42:39 -0500

    answer question 2

--- 2. factorial/factorial.js
+++ 2. factorial/factorial.js
@@ -2,3 +2,32 @@ Using the JavaScript language, have the function FirstFactorial(num)
 take the num parameter being passed and return 
 the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). 
 For the test cases, the range will be between 1 and 18. 
+
+
+function FirstFactorial(num) {
+	function factorial(n) {
+		if(n === 0){
+			return 1;
+		}
+		else{
+			return factorial(n-1) * n;
+		}
+	}	
+
+		return factorial(num);
+	}
+	
+
+FirstFactorial(4);
+
+
+
+
+
+
+Things to think about: 
+When dealing with factorials, thinks recursions. 
+Also think of the following three:
+T - Termination Condition - use this to prevent infinite recursions
+B - Base Case - this statement stops the recursions
+R - Recursion case - the is where the recursion actually happens. 
