a41853039c7cead3fe4944b1d3574622eba11e68
abby-garcia <acg361@gmail.com>
2015-11-05 12:27:18 -0500

    solve problem #4/start 5

--- 5. simple adding/simple-adding.js
+++ 5. simple adding/simple-adding.js
@@ -2,3 +2,15 @@ Using the JavaScript language, have the function SimpleAdding(num)
 add up all the numbers from 1 to num.
 For the test cases, the parameter num will be any 
 number from 1 to 1000. 
+
+function SimpleAdding(num){
+	var number = 0;                      
+	for (var i = 1; i <=num; i++){
+		answer = answer + i;
+	}
+
+	return answer;
+
+}
+
+SimpleAdding(4);
