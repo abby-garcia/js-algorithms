1a744212290fb41681b470931c37ce9c4be361d0
abby-garcia <acg361@gmail.com>
2015-11-06 13:49:54 -0500

    answer 6

--- 6. letter capitalize/capitalize.js
+++ 6. letter capitalize/capitalize.js
@@ -7,9 +7,16 @@ Words will be separated by only one space.
 function LetterCapitalize(str){
 	return str.replace(/\b[a-z]/gi,function(char){
 		return char.toUpperCase();
-
-
 	});
+
+	for (var i = 0; i < words.length; i++) {
+    words[i] = words[i].substring(0,1).toUpperCase() + words[i].substring(1);
+  }
+
+  // return the array of words joined into a string
+  return words.join(" ");
+         
+}
 
 }
 
