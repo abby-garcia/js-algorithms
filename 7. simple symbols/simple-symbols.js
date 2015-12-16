0ee375181a4edd3adac819d8d4f42cf3e5867267
abby-garcia <acg361@gmail.com>
2015-12-16 12:07:54 -0500

    7

--- 7. simple symbols/simple-symbols.js
+++ 7. simple symbols/simple-symbols.js
@@ -1,35 +1,28 @@
-Using the JavaScript language, have the function SimpleSymbols(str) 
-take the str parameter being passed and determine if 
-it is an acceptable sequence by either returning the 
-string true or false. The str parameter will be 
-composed of + and = symbols with several letters 
-between them (ie. ++d+===+c++==a) and for the string 
-to be true each letter must be surrounded by a + symbol. 
-So the string to the left would be false. The string will 
-not be empty and will have at least one letter. 
+// Using the JavaScript language, have the function SimpleSymbols(str) 
+// take the str parameter being passed and determine if 
+// it is an acceptable sequence by either returning the 
+// string true or false. The str parameter will be 
+// composed of + and = symbols with several letters 
+// between them (ie. ++d+===+c++==a) and for the string 
+// to be true each letter must be surrounded by a + symbol. 
+// So the string to the left would be false. The string will 
+// not be empty and will have at least one letter. 
 
 
-
-function SimpleSymbols(str) { 
-
- 
-  var str = '=' + str + '=';
-  for (var i = 0; i < str.length; i++) {
-    
-    
-    if (str[i].match(/[a-z]/i) !== null) {
-
-
-      if (str[i-1] !== '+' || str[i+1] !== '+') { 
-        return false;
-      }
-
+function SimpleSymbols(str) {
+    for(var i = 0; i < str.length; i++){
+        if(str[i + 1] !== "+" || str[i - 1] !== "+"){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
- 
-  }
-
-  return true;
-  
 }
-   
-SimpleSymbols("+d+=3=+s+");                                                                                                
+
+
+SimpleSymbols("++d+===+c++==+a+");
+
+// you have to do a for loop to go thru the string 
+//then you have to figure out what is next to it and the right of it 
+// i -1  & i + 1
