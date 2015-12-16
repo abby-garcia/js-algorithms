921433feb26a138482aa14102f7d9deb0027159b
abby-garcia <acg361@gmail.com>
2015-12-16 11:08:53 -0500

    10

--- 10. alphabet soup/alphabet-soup.js
+++ 10. alphabet soup/alphabet-soup.js
@@ -1,29 +1,23 @@
-Using the JavaScript language, have the 
-function AlphabetSoup(str) take the str 
-string parameter being passed and return 
-the string with the letters in alphabetical order 
-(ie. hello becomes ehllo). 
-Assume numbers and punctuation
- symbols will not be included in the string. 
+// Using the JavaScript language, have the 
+// function AlphabetSoup(str) take the str 
+// string parameter being passed and return 
+// the string with the letters in alphabetical order 
+// (ie. hello becomes ehllo). 
+// Assume numbers and punctuation
+//  symbols will not be included in the string. 
 
 
 
-function AlphabetSoup(str) { 
-  
-  // Use .split to turn the string into an array... 
-  str = str.split("");
-  
-  // .sort to alphabatize the new array...
-  str = str.sort();
-  
-  // and .join to turn it back into a string.
-  str = str.join("");
-  
-  // Finally, we return our answer.
-  return str;
+function AlphabetSoup(str){
+   var orderedLetter = "";
+   orderedLetter = str.split("").sort().join("");
+   return orderedLetter;
 }
 
-// With Chaining
-function AlphabetSoup(str) { 
-  return str.split("").sort().join("");
-}
+AlphabetSoup("zebras");
+
+
+//whenever you're asked to out things in alphabetical order, you
+// will need to split up the letters, sort them, and then join them
+//back together.
+
