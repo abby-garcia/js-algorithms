f5bb0a5c9e8a2af4e81424b023a8c295e7f2b23f
abby-garcia <acg361@gmail.com>
2015-12-23 15:29:16 -0500

    palindrome

--- 15. palindrome/palindrome.js
+++ 15. palindrome/palindrome.js
@@ -1,16 +1,23 @@
+Using the JavaScript language, have the function Palindrome(str)
+take the str parameter being passed and return the 
+string true if the parameter is a palindrome, 
+(the string is the same forward as it is backward) 
+otherwise return the string false. 
+For example: "racecar" is also "racecar" backwards. 
+Punctuation and numbers will not be part of the string. 
 
-function Palindrome(str) {
 
-    str = str.replace(/[\s]/g, "");
+function Palindrome(str){
+	var reverseWord = "";
+	reverseWord = str.split("").reverse().join("");
 
-   
-    strRev = str.split("").reverse().join("");
+	if(str === reverseWord){
+		return true
+	}
+	else{
+		return false;
+	}
 
-   
-    return str == strRev;
 }
 
-
-
-    return str.replace(/[\s]/g, "").toLowerCase() == str.replace(/[\s]/g, "").toLowerCase().split("").reverse().join("");
-}
+Palindrome("racecar");
