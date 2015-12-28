16f4028de89738b8030ad1be1c65b646df1bfa6e
abby-garcia <acg361@gmail.com>
2015-12-27 21:43:53 -0500

    19

--- 18. letter count I/letter-count.js
+++ 18. letter count I/letter-count.js
@@ -1,29 +1,37 @@
-function LetterCountI(str) {
+// Using the JavaScript language, have the 
+// function LetterCountI(str) take the str parameter 
+// being passed and return the first word with the greatest number of 
+// repeated letters. For example: "Today, is the greatest day ever!" 
+// should return greatest because it has 2 e's (and 2 t's) and 
+// it comes before ever which also has 2 e's. If there are 
+// no words with repeating letters return -1. 
+// Words will be separated by spaces. 
 
-    var words = str.split(" ");
-    var largestDif = 0;
-    var answer;
 
-    for (var i = 0; i < words.length; i++) {
-        var currentWord = words[i];
-        var currentWordLength = words[i].length;
-        var currentWordSorted = words[i].split("").sort();
-        for (var j = 0; j < (words[i].length - 1); j++) {
-            if (currentWordSorted[j] === currentWordSorted[j + 1]) {
-                currentWordSorted.splice(j, 1);
-            }
-            var currentDif = (currentWordLength - currentWordSorted.length);
-            if (currentDif > largestDif) {
-                largestDif = currentDif;
-                answer = currentWord;
-            }
-        }
+function LetterCountI(str){
+    var splitString = str.split(" ");
+
+    for(var i = 0; i < splitString.length; i++){
+
+
+
+
+
     }
-    
-    if (largestDif > 0) {
-        return answer;
-    } else {
-        return -1;
-    }
+
+
+
+
+else{
+    return "-1";
+}
+
 
 }
+
+
+LetterCountI("Today, is the greatest day ever!");
+
+
+
+//return the first word with the greatest number of repeated letters
---
+++ 19. second greatlow/second-greatlow.js
@@ -0,0 +1,33 @@
+
+// Using the JavaScript language, have the function SecondGreatLow(arr) 
+// take the array of numbers stored in arr and return the second 
+// lowest and second greatest numbers, respectively, separated by a space. 
+// For example: if arr contains [7, 7, 12, 98, 106] the output should be 
+// 12 98. The array will not be empty and will contain at least 2 numbers. 
+// It can get tricky if there's just two numbers! 
+
+
+function SecondGreatLow(arr){
+
+	arr = arr.sort(function(a, b) {return a - b;});
+
+	for()
+
+	
+
+
+
+
+
+}
+
+
+SecondGreatLow([7, 7, 12, 98, 106]);
+
+
+//things we learned: .sort() can only be used for the alphabet 
+//use the following: arr = arr.sort(function(a, b) {return a - b;});
+
+//we have to loop and make sure there are no REPEATS!!! 
+
+
