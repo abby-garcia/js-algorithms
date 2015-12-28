f1399b92ec42428300f36a001b50b5a6dea6f768
abby-garcia <acg361@gmail.com>
2015-12-27 23:10:08 -0500

    jumping

--- 23.dash insert/dash-insert.js
+++ 23.dash insert/dash-insert.js
@@ -1,17 +1,37 @@
-Using the JavaScript language, 
-have the function DashInsert(str) 
-insert dashes ('-') between each two odd numbers in str. 
-For example: if str is 454793 the output should be 
-4547-9-3. Don't count zero as an odd number. 
+// Using the JavaScript language, 
+// have the function DashInsert(str) 
+// insert dashes ('-') between each two odd numbers in str. 
+// For example: if str is 454793 the output should be 
+// 4547-9-3. Don't count zero as an odd number. 
 
 
 function DashInsert(str){
+	var splitStr = str.split("");
+	var even = splitStr[i] % 2 === 0;
+	var odd = splitStr[i] % 2 !== 0
+
+
+
+	for(var i = 0; i < str.lenght; i++){
+		if
+
+
+
+	}
+
 
 
 
 }
 
+
+
+
 DashInsert(str);
 
 //add dashes between two odd numers in a str. 
+// do a for loop. if there is a odd number befor or after 
+//a number in the middle, put a dash
 
+//if the number can NOT be divided by 2, it's odd 
+//split it up 
