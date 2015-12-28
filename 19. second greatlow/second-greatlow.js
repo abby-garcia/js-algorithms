6a2ff2acc24593c41c31a69ef3eb13709aa0f0d8
abby-garcia <acg361@gmail.com>
2015-12-27 21:57:30 -0500

    19 answer

--- 19. second greatlow/second-greatlow.js
+++ 19. second greatlow/second-greatlow.js
@@ -11,14 +11,20 @@ function SecondGreatLow(arr){
 
 	arr = arr.sort(function(a, b) {return a - b;});
 
-	for()
+	for(var i = arr.length - 1; i > 0; i--){
+		if(arr[i] == arr[i-1]){
+			arr.splice(i,1); //array.splice(index, howMany)
+		}
+	}
 
+	if (arr.length > 2) {
+        return arr[1] + " " + arr[arr.length - 2];
+    } else if (arr.length == 2) {
+        return arr[1] + " " + arr[0];
+    } else {
+        return arr[0] + " " + arr[0];
+    }
 	
-
-
-
-
-
 }
 
 
