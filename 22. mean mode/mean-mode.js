7620de593b10a1138132b6089bcc8f74def550c1
abby-garcia <acg361@gmail.com>
2015-12-27 22:58:49 -0500

    jumping around

---
+++ 22. mean mode/mean-mode.js
@@ -0,0 +1,24 @@
+// Using the JavaScript language, 
+// have the function MeanMode(arr) take the array of 
+// numbers stored in arr and return 1 if the mode 
+// equals the mean, 0 if they don't equal each other 
+// (ie. [5, 3, 3, 3, 1] should return 1 because the 
+// 	mode (3) equals the mean (3)). 
+// The array will not be empty, will only contain 
+// positive integers, and will not contain more than one mode. 
+
+
+function MeanMode(arr){
+
+	
+
+}
+
+
+MeanMode(arr);
+
+
+// mode = most repeated
+// mean = average
+// if mode === mean , return 1 
+// return 0 if they don't equal 
