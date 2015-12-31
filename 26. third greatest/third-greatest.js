697d665c489084e5f68bd87547df54239d835012
abby-garcia <acg361@gmail.com>
2015-12-30 23:52:27 -0500

    power of 2

--- 26. third greatest/third-greatest.js
+++ 26. third greatest/third-greatest.js
@@ -1,19 +1,22 @@
-Using the JavaScript language, have the function ThirdGreatest(strArr) 
-take the array of strings stored in strArr and return the third largest 
-ord within in. So for example: if strArr is 
-["hello", "world", "before", "all"] 
-your output should be world because
- "before" is 6 letters long, and "hello" and "world" are both 5, 
- but the output should be world because it appeared as the last 5 
- letter word in the array. If strArr was 
- ["hello", "world", "after", "all"] the output should be after 
- because the first three words are all 5 letters long, 
- so return the last one. 
- The array will have at least three strings and each string will 
- only contain letters. 
+// Using the JavaScript language, have the function ThirdGreatest(strArr) 
+// take the array of strings stored in strArr and return the third largest 
+// ord within in. So for example: if strArr is 
+// ["hello", "world", "before", "all"] 
+// your output should be world because
+//  "before" is 6 letters long, and "hello" and "world" are both 5, 
+//  but the output should be world because it appeared as the last 5 
+//  letter word in the array. If strArr was 
+//  ["hello", "world", "after", "all"] the output should be after 
+//  because the first three words are all 5 letters long, 
+//  so return the last one. 
+//  The array will have at least three strings and each string will 
+//  only contain letters. 
 
 
  function ThirdGreatest(strArr){
+ 	strArr = strArr.toSplit("");
+
+ 	for(var i = 0; )
 
 
  }
@@ -22,5 +25,8 @@ your output should be world because
  ThirdGreatest(strArr);
 
  //use for loop to go thru each word in the array
+ //count the last largest word. 
  //count each letter in the array
- 
+
+ //split it 
+ //
---
+++ 27. powers of two/twos.js
@@ -0,0 +1,28 @@
+Using the JavaScript language, have the function PowersofTwo(num) 
+take the num parameter being passed which will be an integer 
+and return the string true if it's a power of two. If it's not 
+return the string false. 
+For example if the input is 16 then your program should return 
+the string true but if the input is 22 then the output should be 
+the string false. 
+
+function PowersofTwo(num) { 
+
+  var power = 0;
+  var counter = 1;
+  
+  // raise 2 to the power of 1, 2, 3, etc.
+  // until we reach a number greater than or equal to num
+  while (power < num) {
+    power = Math.pow(2, counter);  
+    counter++;
+  }
+  
+  // determine if 2 to some power is equal to num
+  return (num === power) ? true : false;
+         
+}
+   
+PowersofTwo(512);                                                                                                 
+
+
