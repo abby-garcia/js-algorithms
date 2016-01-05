517f2e544467bd4bc9ada1baea2f4f2af1d7cdcf
abby-garcia <acg361@gmail.com>
2016-01-05 12:42:07 -0500

    mediums

---
+++ 3. Medium Prime Mover/prime-mover.js
@@ -0,0 +1,23 @@
+Using the JavaScript language, have the function PrimeMover(num) 
+return the numth prime number. The range will be from 1 to 10^4. 
+For example: if num is 16 the output should be 53 as 53 is the 16th prime number. 
+
+
+
+
+function PrimeMover(num) {
+	var counter = 0;
+
+	
+
+
+
+
+
+}
+
+
+PrimeMover(num);
+
+//when it is a prime number, brint up the counter
+//
